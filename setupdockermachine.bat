8a4040b609c31044c1485efba5ea508435c0047b
Dan <33978411+Dan-945@users.noreply.github.com>
2023-12-30 11:19:00 +0100

    Update setupdockermachine.bat

--- setupdockermachine.bat
+++ setupdockermachine.bat
@@ -38,3 +38,59 @@ sudo smbpasswd -a dm
 sudo service smbd restart
 
 fi
+
+#list what packages to install from package manager
+dev_packages=(
+    'zsh'
+    'vim'    
+)
+
+#Function to check if package already installed and if not install it.
+install_dev(){
+  for package in "${dev_packages[@]}"
+  do
+    echo "Installing $package"
+    if ! command -v  $package &>/dev/null; then
+     # install_$package
+        sudo apt update && sudo apt upgrade
+        sudo apt install $package
+  echo "$package installed"
+    else
+
+     echo  "$package already installed"
+    fi
+  done
+}
+
+#run function for installing packages
+install_dev
+
+
+echo "mounting network shares"
+ if [ -d "/mnt/docker_configs" ]
+  then
+          echo "Folder already exists, will not create"
+  else
+          echo "does not exist will create"
+          mkdir "/mnt/docker_configs"
+  fi
+
+if [ $(grep "//192.168.1.198/configs/ /mnt/docker_configs" -c /etc/fstab) -ge 1 ]
+then 
+echo "its already there"
+else 
+ cat >> /etc/fstab << EOL
+//192.168.1.198/configs/ /mnt/docker_configs/ cifs username=dm,password=linux,rw,iocharset=utf8,file_mode=0777
+//192.168.1.161/Downloads/ /mnt/Downloads/ cifs username=dm,password=linux,rw,iocharset=utf8,file_mode=0777,dir_mode=0777,noperm
+//192.168.1.161/Pool4Configs/ /mnt/Pool4Configs/ cifs username=dm,password=linux,rw,iocharset=utf8,file_mode=0777,dir_mode=0777,noperm
+//192.168.1.161/Pool1Media/ /mnt/Pool1Media/ cifs username=dm,password=linux,rw,iocharset=utf8,file_mode=0777,dir_mode=0777,noperm
+
+//192.168.1.161/Pool2Media/ /mnt/Pool2Media/ cifs username=dm,password=linux,rw,iocharset=utf8,file_mode=0777,dir_mode=0777,noperm
+
+>
+EOL
+echo "its missing"
+fi
+
+mount -a
+
